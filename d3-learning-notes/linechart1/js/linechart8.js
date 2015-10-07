64a44a9785f67534b0e3bdff3821417db0dacd76
jianganglu <1055375563@qq.com>
2015-10-07 08:06:49 +0800

    Initial Commit

---
+++ d3-learning-notes/linechart1/js/linechart8.js
@@ -0,0 +1,46 @@
+﻿/**
+ * @name linechart8
+ * @desc linechart8
+ * @auth Jiangang Lu
+ * @date 2015/10/06
+ */
+
+;(function() {
+  var Data;
+
+  var table = d3.select('body')
+    .append('table')
+    .attr('class', 'table');
+  var thead = table.append('thead'),
+      tbody = table.append('tbody');
+
+  var reload = function() {
+    d3.csv('../data/villains.csv', function(data) {
+      Data = data;
+      Data = Data.filter(function(d) { return d['Doctor actor'] == 'Matt Smith'; });
+      redraw();
+    });
+  };
+  reload();
+
+  var redraw = function() {
+    var tr = tbody.selectAll('tr').data(Data)
+    tr.enter().append('tr');
+    tr.exit().remove();
+
+    tr.selectAll('td')
+      .data(function(d) { return d3.values(d); }) // d3.values(d)可以把对象转成数据
+      .enter()
+      .append('td')
+      .text(function(d) { return d; });
+
+    tbody.selectAll('tr')
+      .sort(function(a, b) { return d3.ascending(a['Year first'], b['Year first']); }); // d3.ascending(a['Year first'], b['Year first']); 升序
+    tbody.selectAll('tr').sort(function (a, b) {
+      return d3.descending(Number(a['Doc. no.']), Number(b['Doc. no.']));
+    });
+    tbody.selectAll('tr')
+      .filter(function(d) { return d['Doctor actor'] != 'Matt Smith'; })
+      .remove();
+  }
+})();
---
+++ d3-learning-notes/linechart1/js/linechart9.js
@@ -0,0 +1,72 @@
+﻿var svg = d3.select('body').append('svg')
+  .attr('width', '100%')
+  .attr('height', '100%');
+
+svg.append('text')
+  .text('A picture!')
+  .attr({
+    x: 10,
+    y: 150,
+    'text-anchor': 'start'
+  });
+
+svg.append('line')
+  .attr({
+    x1: 10,
+    y1: 10,
+    x2: 100,
+    y2: 100,
+    stroke: 'blue',
+    'stroke-width': 3
+  });
+
+svg.append('rect')
+  .attr({
+    x: 200,
+    y: 50,
+    width: 300,
+    height: 400
+  });
+
+svg.select('rect')
+  .attr({
+    stroke: 'green',
+    'stroke-width': 0.5,
+    fill: 'white',
+    rx: 20,
+    ry: 40
+  });
+
+svg.append('circle')
+  .attr({
+    cx: 350,
+    cy: 250,
+    r: 100,
+    fill: 'green',
+    'fill-opacity': 0.5,
+    stroke: 'steelblue',
+    'stroke-width': 2
+  });
+
+svg.append('ellipse')
+  .attr({
+    cx: 350,
+    cy: 250,
+    rx: 150,
+    ry: 70,
+    fill: 'green',
+    'fill-opacity': 0.3,
+    stroke: 'steelbule',
+    'stroke-width': 0.7
+  });
+
+svg.append('ellipse')
+  .attr({
+    cx: 350,
+    cy: 250,
+    rx: 20,
+    ry: 70
+  });
+
+svg.selectAll('ellipse, circle')
+  .attr('transform', 'translate(150, 0) scale(1.2) translate(-70, 0) rotate(-45, '+ (350/1.2) +', '+ (250/1.2) +') skewY(20) ');
